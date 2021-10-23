fd959b5fd995bb581da60c482b97a9ba3af3f338
suhardiyan <nasihun.amin93@gmail.com>
2021-10-23 20:52:53 +0700

    revisee custom order

--- models/domain/custom_order.go
+++ models/domain/custom_order.go
@@ -9,24 +9,35 @@ type CustomOrder struct {
 	Number string `json:"number"`
 	StoreId int `json:"store_id"`
 	ProductId int `json:"product_id"`
+	SellerId int `json:"seller_id"`
+	TypeCakeId int `json:"type_cake_id"`
 	CakeCharacter string `json:"cake_character"`
 	CakeShape string `json:"cake_shape"`
-	CakeSize int `json:"cake_size"`
+	CakeSize string `json:"cake_size"`
+	VariantCakeId int `json:"variant_cake_id"`
 	DeliveryDate time.Time `json:"delivery_date"`
-	Price float64 `json:"price"`
 	Other string `json:"other"`
 	CakeCustomName string `json:"cake_custom_name"`
-	AdditionalPrice float64 `json:"additional_price"`
 	Candle string `json:"candle"`
+	ShipmentType string `json:"shipment_type"`
+	Status string `json:"status"`
+	ProductionNote string `json:"production_note"`
+	CustomerName string `json:"customer_name"`
+	CustomerPhone string `json:"customer_phone"`
+	CustomerAddress string `json:"customer_address"`
+	CustomerAddressDescription string `json:"customer_address_description"`
+	RecipientName string `json:"recipient_name"`
+	RecipientPhone string `json:"recipient_phone"`
+	RecipientAddress string `json:"recipient_address"`
+	RecipientAddressDetail string `json:"recipient_address_detail"`
+	Price float64 `json:"price"`
+	AdditionalPrice float64 `json:"additional_price"`
 	Discount float64 `json:"discount"`
 	DeliveryCost float64 `json:"delivery_cost"`
-	RecipientName string `json:"recipient_name"`
-	RecipientAddress string `json:"recipient_address"`
-	RecipientMethod string `json:"recipient_method"`
-	RecipientStatus string `json:"recipient_status"`
-	ShipmentType string `json:"shipment_type"`
-	Note string `json:"note"`
-	Status string `json:"status"`
+	Total float64 `json:"total"`
+	PaymentMethodId int `json:"payment_method_id"`
+	DownPayment float64 `json:"down_payment"`
+	PaymentNote string `json:"payment_note"`
 	CreatedBy int `json:"created_by"`
 	CreatedAt time.Time`json:"created_at,omitempty" example:"2020-03-16T13:55:09.598136+07:00"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" example:"2020-03-16T13:55:09.598136+07:00"`
--- models/web/custom_order_datatable.go
+++ models/web/custom_order_datatable.go
@@ -3,11 +3,11 @@ package web
 import "kalika-be/models/domain"
 
 type CustomOrderDatatable struct {
-	StoreId                int    `json:"store_id"`
-	StoreName              string `json:"store_name"`
-	ProductId                int    `json:"product_id"`
-	ProductName              string `json:"product_name"`
+	StoreId     int    `json:"store_id"`
+	StoreName   string `json:"store_name"`
+	ProductId   int    `json:"product_id"`
+	ProductName string `json:"product_name"`
 	domain.CustomOrder
 	CreatedByName string `json:"created_by_name"`
-	Action string `json:"action"`
+	Action        string `json:"action"`
 }
